8781a690024d74e59040bd7242ecf8a1c5794083
psm <1828421@donga.ac.kr>
2023-09-11 19:12:18 +0900

    v2.0 blur update commit

---
+++ src/complete2.py
@@ -0,0 +1,97 @@
+import pyaudio
+import os
+from curse_detector import CurseDetector
+from google.cloud import speech_v1p1beta1 as speech
+from pydub import AudioSegment
+
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100
+
+weights_paths = ['C:/hw/Curse-detection-v2/src/models/weights6.h5']
+curse = CurseDetector(weights_paths)
+
+def listen_print_loop(responses):
+    for response in responses:
+        start_idx=int(0)
+        end_idx= int(0)
+        if not response.results:
+            continue
+
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+        
+        # Check if this result is the final result for this portion of the audio.
+        if result.is_final:
+            text,filter_word,word_list = curse.masking(result.alternatives[0].transcript)
+            print("filter : " + str(text))
+            print("origin : {}".format(result.alternatives[0].transcript))
+            # Get start and end time of each word
+            for word_info in result.alternatives[0].words:
+                end_idx += len(word_info.word)
+                start_time = word_info.start_time.total_seconds()
+                end_time = word_info.end_time.total_seconds()
+                
+                # If the word is a curse, blur it in the original audio
+                for i in range(0,len(filter_word)):
+                    if ((filter_word[i][0]<end_idx) and (filter_word[i][0]>=start_idx)) or ((filter_word[i][0]+filter_word[i][1]<end_idx) and (filter_word[i][0]+filter_word[i][1]>=start_idx)) :
+                        blur_audio(start_time,end_time)
+                start_idx = end_idx
+
+def blur_audio(start_sec, end_sec):
+    """Blurs audio between start and end times."""
+    audio_path="./path_to_your_audio_file.wav"
+    
+    audio = AudioSegment.from_file(audio_path)
+    
+    start_millisec = int(start_sec * 1000)
+    end_millisec = int(end_sec * 1000)
+
+    before_blur_part=audio[:start_millisec]
+    blur_part=audio[start_millisec:end_millisec].low_pass_filter(10)
+    after_blur_part=audio[end_millisec:]
+
+    final_audio=before_blur_part+blur_part+after_blur_part
+    
+    final_audio.export("blurred_"+audio_path, format="wav")
+
+def main():
+  
+    client=speech.SpeechClient()
+
+    config=speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=RATE,
+    language_code="ko-KR",
+    enable_word_time_offsets=True,
+    )
+
+    streaming_config=speech.StreamingRecognitionConfig(
+    config=config,
+    interim_results=True,   # We still want interim results so we can get results while audio is still being recorded.
+    )
+
+    mic=pyaudio.PyAudio()
+
+    stream=mic.open(
+    format=pyaudio.paInt16,
+    channels=1,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=CHUNK)
+
+    while True:   # Add an infinite loop to keep recording and transcribing.
+        audio_generator =(stream.read(CHUNK) for _ in range(0, int(RATE / CHUNK * 5))) 
+
+        requests =(speech.StreamingRecognizeRequest(audio_content=data)
+            for data in audio_generator)
+
+        responses = client.streaming_recognize(streaming_config, requests)
+        
+        listen_print_loop(responses)
+
+if __name__ == "__main__":
+    text,word,word_list= curse.masking("loding complete",flag=False)
+    print(text)
+    main()
--- src/console_V1.py
+++ src/console_V1.py
@@ -6,8 +6,9 @@ import pyaudio
 import numpy as np
 import mysql.connector
 from datetime import datetime
-import math
-import time
+from google.cloud import speech
+from pydub import AudioSegment
+
 
 def save_to_db(uid, origin_text, filter_text, score):
     try:
@@ -35,6 +36,7 @@ def save_to_db(uid, origin_text, filter_text, score):
     return True
 
 def get_threshold(stream, chunk, RATE,seconds=5):
+    print("소음치 측정 중")
     audio_energy_values = []
 
     for _ in range(int(seconds / chunk * RATE)):
@@ -94,50 +96,84 @@ def record_audio(output_file, stop_when_silence=3):
         wf.setframerate(RATE)
         wf.writeframes(b''.join(frames))
         
+def blur_audio(start_sec, end_sec):
+    """Blurs audio between start and end times."""
+    audio_path="test.wav"
+    
+    audio = AudioSegment.from_file(audio_path)
+    
+    start_millisec = int(start_sec * 1000)
+    end_millisec = int(end_sec * 1000)
 
-def recognize_speech(filename,curse):
-    recognizer = sr.Recognizer()
-    with sr.AudioFile(filename) as source:
-        audio_data = recognizer.record(source)
+    before_blur_part=audio[:start_millisec]
+    silence_duration = end_millisec - start_millisec
+    silence_segment = AudioSegment.silent(duration=silence_duration)
+    after_blur_part=audio[end_millisec:]
+
+    final_audio=before_blur_part + silence_segment + after_blur_part
+    
+    final_audio.export(audio_path, format="wav")
+
+
+def recognize_speech(filename, curse):
+    client = speech.SpeechClient()
+
+    with open(filename, "rb") as audio_file:
+        input_audio = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=input_audio)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code="ko-KR",
+        enable_word_time_offsets=True,
+    )
 
     try:
         print("음성을 인식 중입니다...")
-        start = time.time()
         
-        text = recognizer.recognize_google(audio_data, language='ko-KR')
-        end = time.time()
-        print(f"{end - start:.5f} sec")
-        print("Here's the text from the audio:")
-        ensemble = curse.ensemble(text)
-        masking = curse.masking(text)
-        print(ensemble)
-        print(masking)
-        save_to_db("1", text,masking,ensemble)
-    except Exception:
-        print("다시 녹음해주세요")
+        response = client.recognize(config=config, audio=audio)
+        start_idx=int(0)
+        end_idx= int(0)
+        for result in response.results:
+            text = result.alternatives[0].transcript
+            
+            print("Here's the text from the audio:")
+            
+            text,filter_word,word_list = curse.masking(result.alternatives[0].transcript)
+            print(text)
+            print("filter : " + str(text))
+            print("origin : {}".format(result.alternatives[0].transcript))
+            for word_info in result.alternatives[0].words:
+                end_idx += len(word_info.word)
+                start_time = word_info.start_time.total_seconds()
+                end_time = word_info.end_time.total_seconds()
+                
+                # If the word is a curse, blur it in the original audio
+                for i in range(0,len(filter_word)):
+                    if ((filter_word[i][0]<end_idx) and (filter_word[i][0]>=start_idx)) or ((filter_word[i][0]+filter_word[i][1]<end_idx) and (filter_word[i][0]+filter_word[i][1]>=start_idx)) :
+                        blur_audio(start_time,end_time)
+                start_idx = end_idx
+    except Exception as e:
+        print(f"An error occurred: {e}")
     
 
 
 
 
 if __name__ == "__main__":
-    output_file = "recorded_audio.wav"
+    output_file = "test.wav"
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    weights_paths = ['C:/hw/Curse-detection-v2/src/models/weights6.h5']
+    weights_paths = ['./src/models/weights6.h5']
 
     curse = CurseDetector(weights_paths)
-    i=0
-
     while(True):
-        if(i==0):
-            print(curse.masking("loding complete"))
-        
-        i=1
-        output_file = "recorded_audio.wav"
+        curse.masking("loding complete",flag=False)
 
         # 음성 녹음 및 저장
         record_audio(output_file)
         
         # 저장된 음성 파일을 텍스트로 변환
         recognize_speech(output_file,curse)
+        break
         
